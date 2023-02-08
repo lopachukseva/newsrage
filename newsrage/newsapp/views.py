ed44050f03669f40aea962ad1c10c14ae6c91158
Seva Lopachuk <lopachuk.seva@gmail.com>
2023-02-08 21:48:28 +0200

    add urlpatterns, news model, views changed settings

--- newsrage/newsapp/views.py
+++ newsrage/newsapp/views.py
@@ -1,3 +1,14 @@
+from django.http import HttpResponse, HttpResponseNotFound
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(response):
+    return HttpResponse('Стартовая страница сайта')
+
+
+def categories(response, category_id):
+    return HttpResponse(f'Категория новостей: {category_id}')
+
+
+def pageNotFound(response, exception):
+    return HttpResponseNotFound(f'<h3>Упс! Кажется, такой страницы не существует, или же она была удалена.</h3>')
