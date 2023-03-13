edbbc23ccc0f0efae0cd6f20b38b8e5df41c8972
Seva Lopachuk <lopachuk.seva@gmail.com>
2023-03-13 13:29:14 +0100

    add url patterns for login/logout/register

--- newsrage/newsapp/urls.py
+++ newsrage/newsapp/urls.py
@@ -6,6 +6,9 @@ urlpatterns = [
     path('', index, name='index'),
     path('contacts/', contacts, name='contacts'),
     path('feedback/', feedback, name='feedback'),
+    path('login/', LoginUser.as_view(), name='login_user'),
+    path('register/', RegisterUser.as_view(), name='register_user'),
+    path('logout/', logout_user, name='logout_user'),
     path('category/<slug:category_slug>/', category, name='category'),
     path('<slug:post_slug>/', post, name='post'),
 ]
